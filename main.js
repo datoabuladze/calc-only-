306478ad73c56cedb5c4b2781eb9a3cf822d752c
datoabuladze <dato_abuladze@mail.ru>
2026-03-17 16:53:43 +0400

    sruli

---
+++ main.js
@@ -0,0 +1,6 @@
+function main() {
+  let num1 = document.getElementById("number1").value;
+  let num2 = document.getElementById("number2").value;
+  let jami = (document.getElementById("pas").innerHTML =
+    Number(num1) + Number(num2));
+}
